7fd1a5a4eefc5535ca5534b5ce3bdc5cc34e8a6f
Kimchaewook <allurings2@naver.com>
2022-08-24 13:06:41 +0900

    test: test Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 import ProductList from './components/Product/ProductList'
 
 function App() {
+  const Fonts = styled.p`
+    font-family: 'Wooridaum';
+  `
   return (
     <div className="App">
       <ProductList />
+
+      <Fonts>안녕하세요</Fonts>
+      <div>안녕하세요</div>
     </div>
   )
 }
