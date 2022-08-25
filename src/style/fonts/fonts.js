0595440ce57ef181fcbfce2c29a43dcdeb4f3649
Kimchaewook <allurings2@naver.com>
2022-08-25 13:05:08 +0900

    test: font 테스트

--- src/style/fonts/fonts.js
+++ src/style/fonts/fonts.js
@@ -3,19 +3,22 @@ import { createGlobalStyle } from "styled-components"
 export default createGlobalStyle`
   @font-face {
     font-family: "Wooridaum";
-    src: url('./WooridaumL.ttf');
+    src: url('./WooridaumL.ttf') format('truetype'),
+    url('./WooridaumL.woff') format('woff');
     font-weight: 300;
   }
 
   @font-face {
     font-family: "Wooridaum";
-    src: url('./WooridaumR.ttf');
+    src: url('./WooridaumR.ttf') format('truetype'),
+    url('./WooridaumR.woff') format('woff');
     font-weight: 500;
   }
 
   @font-face {
     font-family: "Wooridaum";
-    src: url('./WooridaumB.ttf');
+    src: url('./WooridaumB.ttf') format('truetype'),
+    url('./WooridaumB.woff') format('woff');
     font-weight: 600;
   }
 `
