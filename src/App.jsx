15ba0f602b28bcb4f687d4fcbda39018622a2057
Kimchaewook <allurings2@naver.com>
2022-08-25 13:04:16 +0900

    feat: import bootstrap 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import styled from 'styled-components'
-import ProductList from './components/Product/ProductList'
-=======
 import ProductList from './components/Product/ProductList/ProductList'
->>>>>>> 4e24ba14e53aa7c7defffbd65405d9e2affe0d16
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const Fonts = styled.p`
-    font-family: 'Wooridaum';
-  `
+
   return (
     <div className="App">
       <ProductList />
-
-      <Fonts>안녕하세요</Fonts>
       <div>안녕하세요</div>
     </div>
   )
