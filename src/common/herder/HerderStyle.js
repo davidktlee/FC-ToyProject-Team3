6a8f6dc7777bc668c480d0bf6dae56f344673d67
Kimchaewook <allurings2@naver.com>
2022-08-25 21:15:54 +0900

    feat: HerderStyled.js 파일 추가

---
+++ src/common/herder/HerderStyle.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+
+export const Herder = styled.div `
+  width: 425px;
+  height: 8vh;
+  margin-bottom: 2em;
+  border-bottom: 1px solid rgb(0, 0, 0, .5);
+`
+
+export const Container = styled.div `
+  width: 400px;
+  height: 8vh;
+  margin: 0 auto;
+  font-size: 2em;
+  font-weight: 600;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Flex = styled.div `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+`
+export const LoginTextContainer = styled.div`
+  height: 100%;
+  font-size: 16px;
+`
+
+export const Login = styled.a `
+  margin: 0 10px;
+  text-decoration: none;
+  color: #0096ff;
+  &:hover {
+    color: rgb(0, 0, 0, .8);
+    cursor: pointer;
+  }
+`
+
+export const Img = styled.img `
+  width: 10%;
+`
+
+
