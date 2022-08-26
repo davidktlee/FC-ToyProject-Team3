7b2bb93c4e18f38cd6e2dd9c5cda4853e5689c0f
SeungHun6450 <toffg6450@naver.com>
2022-08-26 16:41:56 +0900

    design: 회원가입 페이지 수정: 헤더 추가, 로고 삭제, 직업 목록 수정

--- src/components/sign/signUp/SignUp.jsx
+++ src/components/sign/signUp/SignUp.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import * as S from './SignUpStyle'
 import Button from 'react-bootstrap/Button'
+import Header from '../../../common/herder/Herder'
 
 function SignUp() {
   return (
     <S.Container>
-      <S.Title>K-금융 대출 로그인</S.Title>
-      <S.LogoImg />
+      <Header />
+      <S.Title>KFB-금융 대출 로그인</S.Title>
       <S.InputArea>
         <S.Name />
         <S.Email />
@@ -24,13 +25,11 @@ function SignUp() {
           </S.Age>
           <S.Job>
             <option value="">직업</option>
-            <option value="군인">군인</option>
             <option value="공무원">공무원</option>
             <option value="직장인">직장인</option>
-            <option value="사업가">사업가</option>
+            <option value="자영업">자영업</option>
             <option value="학생">학생</option>
-            <option value="주부">주부</option>
-            <option value="무직">무직</option>
+            <option value="주부·무직">주부·무직</option>
           </S.Job>
         </S.Select>
       </S.InputArea>
