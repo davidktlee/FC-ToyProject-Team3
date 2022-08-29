d8c1fd52642af3de8e1e12414735106138964b5c
SeungHun6450 <toffg6450@naver.com>
2022-08-29 14:11:07 +0900

    feat: input핸들러 및 버튼 이벤트 임시 추가

--- src/components/sign/signIn/SignIn.jsx
+++ src/components/sign/signIn/SignIn.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './SigninStyle'
 import Button from 'react-bootstrap/Button'
 
 function Signin() {
+  const [userInput, setUserInput] = useState({ email: '', password: '' })
+  const inputChangeHandler = e => {
+    const { name, value } = e.target
+    setUserInput({ ...userInput, [name]: value })
+  }
+
   return (
     <S.Container>
       <S.Title>KFB-금융 대출 로그인</S.Title>
       <S.InputArea>
-        <S.Email />
-        <S.Password />
+        <S.Email name="email" onChange={inputChangeHandler} />
+        <S.Password name="password" onChange={inputChangeHandler} />
       </S.InputArea>
       <div class="d-grid gap-2 col-6 mx-auto">
-        <Button variant="outline-primary" size="lg">
+        <Button variant="outline-primary" size="lg" onClick={() => signIn(userInput)}>
           로그인
         </Button>
       </div>
