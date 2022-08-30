490eb812c7cb824bbcc0b82889a150b15cff54af
ohmygyeol <ohmygyeol2678@gmail.com>
2022-08-30 17:09:58 +0900

    feat: router.jsx 복구

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import LikeProduct from '../components/likeProduct/LikeProduct'
+import ViewMain from '../pages/main/ViewMain.jsx'
+import AllProduceList from '../pages/viewAllProduct/AllProductList'
+import ProductList from '../common/prouctlist/ProductList'
+import SignIn from '../components/sign/signIn/SignIn'
+import SignUp from '../components/sign/signUp/SignUp'
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<ViewUserArea />} />
-      <Route path="/signin" element={<ViewAllArea />} />
+      <Route path="/" element={<ViewMain />}>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/likeproduct" element={<LikeProduct />} />
+        <Route path="/allproductlist" element={<AllProduceList />} />
+      </Route>
     </Routes>
   )
 }
