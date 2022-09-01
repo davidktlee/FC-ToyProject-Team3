9334ab7e73efc3c4c1a0e5bcdeaf9d38f6ec1849
davidktlee <rudxor567@naver.com>
2022-09-01 19:08:55 +0900

    chore: 주석 제거

--- src/common/productlist/ProductList.jsx
+++ src/common/productlist/ProductList.jsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import  * as S from './ProductListStyle';
-// import { useSignInMutation } from '../../api/useApi'
-
-// function ProductList() {
-
-//   const [signin] = useSignInMutation()
-//   console.log(signin.data)
-
-//   return (
-//     <>
-//     <S.InterestedItems>
-//       OOO회원님의 관심 상품
-//     </S.InterestedItems>
-//     <S.Container>
-//       <S.Logo>
-//         <img src="" alt="" />
-//       </S.Logo>
-//       <S.ManualContainer>
-//         <div>{signin}</div>
-//         <div>상품 금액</div>
-//       </S.ManualContainer>
-//     </S.Container>
-//     </>
-//   )
 import React, { useEffect, useState } from 'react'
 import { useGetProductsQuery, useGetUserDataQuery, useGetUserProductsQuery } from '../../api/useApi'
 import TotalAmount from '../../components/totalamount/TotalAmount'
