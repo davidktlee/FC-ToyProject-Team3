838e7b8627b4487fffc54e76421409fbf0775341
Kimchaewook <allurings2@naver.com>
2022-09-01 15:48:21 +0900

    feat: 컴포넌트 연결 및 삼항연산자 추가

--- src/common/ProductItem/ProductItem.jsx
+++ src/common/ProductItem/ProductItem.jsx
@@ -1,53 +1,90 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import * as S from './ProductItemStyle';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
-import { useWishListAddMutation } from '../../api/useApi';
+import { useWishListAddMutation, useWishListDeleteMutation } from '../../api/useApi';
 import Button from 'react-bootstrap/Button';
+import { useCookies } from 'react-cookie'
 
 function ProductItem({ name, loan, logo, productId }) {
+  const location = useLocation()
+  const [cookies] = useCookies()
+  const token = cookies.accessToken
   const [commaLoan, setCommaLoan] = useState()
   const [starState, setStarState] = useState(false)
+  const [starStates, setStarStates] = useState(true)
   const [hoverState, setHoverState] = useState(false)
+  const [hoverStates, setHoverStates] = useState(false)
   const [postLikeProduct, setPostLikeProduct] = useState({data: { name, loan, logo, productId }})
-  const [likeProduct] = useWishListAddMutation()
+  const [likeProduct, setLikeProduct] = useWishListAddMutation()
+  const [deleteLikeProduct, setDeleteLikeProduct] = useWishListDeleteMutation()
+
   const onClickStar = () => {
     setStarState(prev => !prev)
+    setStarStates(prev=> !prev)
   }
   const changeHoverState = () => {
     setHoverState(prev => !prev)
+    setHoverStates(prev => !prev)
   }
   const toComma = () => {
     const addComma = loan.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
     setCommaLoan(addComma)
   }
 
+
   useEffect(() => {
     toComma()
   }, [loan])
   return (
-    <S.Container onMouseEnter={changeHoverState} onMouseLeave={changeHoverState}>
-      <S.Logo>
-        <S.Img src={logo} alt="bank-logo" />
-      </S.Logo>
-      <S.ManualContainer>
-        <div>{name}</div>
-        <div>{commaLoan} 원</div>
-      </S.ManualContainer>
-      <S.Star onClick={() => {
-        onClickStar(),
-        likeProduct({data: postLikeProduct, productId: productId})
-        }}>
-      {starState ? <AiFillStar /> : <AiOutlineStar />}
-      </S.Star>
-      {/* <S.Star onClick={onClickStar}>{starState ? <AiFillStar /> : <AiOutlineStar />}</S.Star>
-      {hoverState ? (
-        <S.PurchaseBtn>
-          <Button variant="outline-primary" size="sm">
-            장바구니 담기
-          </Button>{' '}
-        </S.PurchaseBtn>
-      ) : null} */}
-    </S.Container>
+    <>
+      {location.pathname === '/allproductlist' ? (
+        <S.Container onMouseEnter={changeHoverState} onMouseLeave={changeHoverState}>
+        <S.Logo>
+          <S.Img src={logo} alt="bank-logo" />
+        </S.Logo>
+        <S.ManualContainer>
+          <div>{name}</div>
+          <div>{commaLoan} 원</div>
+        </S.ManualContainer>
+        <S.Star onClick={() => {
+          onClickStar(),
+          likeProduct({data: postLikeProduct, productId: productId, token: token})
+        }}>{starState ? <AiFillStar /> : <AiOutlineStar />}</S.Star>
+        {hoverState ? (
+          <S.PurchaseBtn>
+            <Button variant="outline-primary" size="sm">
+              장바구니 담기
+            </Button>{' '}
+          </S.PurchaseBtn>
+        ) : null}
+      </S.Container>
+      ) : (
+        <>
+          <S.Container onMouseEnter={changeHoverState} onMouseLeave={changeHoverState}>
+            <S.Logo>
+              <S.Img src={logo} alt="bank-logo" />
+            </S.Logo>
+            <S.ManualContainer>
+              <div>{name}</div>
+              <div>{commaLoan} 원</div>
+            </S.ManualContainer>
+            <S.Star onClick={() => {
+              onClickStar()
+              deleteLikeProduct({ productId: productId, token: token})
+            }}> {starStates ? <AiFillStar /> : <AiOutlineStar />}
+            </S.Star>
+            {hoverStates ? (
+              <S.PurchaseBtn>
+                <Button variant="outline-primary" size="sm">
+                  장바구니 담기
+                </Button>{' '}
+              </S.PurchaseBtn>
+        ) : null}
+          </S.Container>
+        </>
+      )}
+    </>
   )
 }
 
