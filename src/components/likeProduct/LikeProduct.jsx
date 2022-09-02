59dad328b08bb8e24b559f0fd21f6ae85d9e57b2
davidktlee <rudxor567@naver.com>
2022-09-02 22:08:22 +0900

    rename: 폴더 이름 수정

--- src/components/likeProduct/LikeProduct.jsx
+++ src/components/likeProduct/LikeProduct.jsx
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
-import * as S from './LikeProductStyle';
-import { useWishListQuery, useGetUserDataQuery } from '../../api/useApi';
-import ProductItem from '../../common/productItem/ProductItem';
-import { useCookies } from 'react-cookie';
-
+import React, { useState } from 'react'
+import * as S from './LikeProductStyle'
+import { useWishListQuery, useGetUserDataQuery } from '../../api/useApi'
+import ProductItem from '../../common/productItem/ProductItem'
+import { useCookies } from 'react-cookie'
 
 function LikeProduct() {
   const [cookies] = useCookies()
-  const token = cookies.accessToken;
-  const { data: whihList, isLoadding} = useWishListQuery(token)
-  const {data : username} = useGetUserDataQuery(token)
-  if (isLoadding) {return <div>로딩중...</div>}
+  const token = cookies.accessToken
+  const { data: whihList, isLoadding } = useWishListQuery(token)
+  const { data: username } = useGetUserDataQuery(token)
+  if (isLoadding) {
+    return <div>로딩중...</div>
+  }
 
   return (
     <>
       <S.User>{username && <S.Username>{username.username}</S.Username>} 회원님 관심상품</S.User>
       <S.ScrollBar>
         {whihList &&
-          whihList.map((list) => {
-            return(
+          whihList.map(list => {
+            return (
               <div key={list.productId}>
-              <ProductItem name={list.name} loan={list.loan} logo={list.logo} productId={list.productId}/>
+                <ProductItem name={list.name} loan={list.loan} logo={list.logo} productId={list.productId} />
               </div>
             )
-          })
-        }
+          })}
       </S.ScrollBar>
     </>
   )
 }
 
 export default LikeProduct
-
