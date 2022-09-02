bcdd10ca4f891ca3255e387f874ffe01413d6490
Kimchaewook <allurings2@naver.com>
2022-09-02 17:56:12 +0900

    peft: 필요없는 코드 삭제

--- src/api/useApi.js
+++ src/api/useApi.js
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { useCookies, Cookies } from 'react-cookie'
 
 export const useApi = createApi({
   reducerPath: 'useApi',
